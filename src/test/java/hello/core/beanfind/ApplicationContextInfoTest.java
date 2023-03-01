bd0ea9857ce2bd7e24a124b2b7b1783bfa8273c3
JinJuJeong <wjdwlswn731@gmail.com>
2023-03-01 20:32:34 +0900

    #8 - 스프링으로 전환하기
    테스트 코드 작성 : 타입 중복 시 Bean 조회 테스트

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -1,13 +1,16 @@
-package hello.core;
+package hello.core.beanfind;
 
+import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class ApplicationContextTest {
+public class ApplicationContextInfoTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
+    /*모든 빈을 출력하게되면 Role 이 Infrastructure 인 Bean 까지 출력되는 것을 볼 수 있다.
+    Infrastructure Bean == 스프링 프레임워크의 기반이 되는, 내부적으로 사용하는 Bean*/
     @Test
     @DisplayName("모든 빈 출력하기")
     void findAllBean() {
@@ -15,7 +18,6 @@ public class ApplicationContextTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
             System.out.println("name = " + beanDefinitionName + "object = " + bean);
-
         }
     }
 
--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,4 +1,4 @@
-package hello.core;
+package hello.core.discount;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
